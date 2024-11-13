e0626f85133fd0c72ca53c26e48bb14a86293dd5
sayan05052000 <samiyasti@gmail.com>
2024-11-13 11:51:45 +0530

    first commit for E commerce

---
+++ config-srever/src/main/java/com/ecommerce/minatiConfectioners/configserver/ConfigSreverApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.minatiConfectioners.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSreverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSreverApplication.class, args);
+	}
+
+}
---
+++ config-srever/src/test/java/com/ecommerce/minatiConfectioners/configserver/ConfigSreverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.minatiConfectioners.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSreverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
